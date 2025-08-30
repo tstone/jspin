654e056a107e79adc9f00818964a4c63fa62cd1d
Titus <wastingtape@gmail.com>
2025-08-30 12:45:32 -0600

    Fix delimiter in triggerDriverCmd return value

--- packages/jspin-core/src/commands/trigger-driver.ts
+++ packages/jspin-core/src/commands/trigger-driver.ts
@@ -4,5 +4,5 @@ export function triggerDriverCmd(driverId: number, mode: 'automatic' | 'manual' 
   const modeValue = mode === 'automatic' ? '0' :
     mode === 'manual' ? '1' :
       mode === 'disconnected' ? '2' : '3';
-  return `TL:${driverId.toString(16)}:${modeValue}\r`;
+  return `TL:${driverId.toString(16)},${modeValue}\r`;
 }
