57b8d6c463ef0f726089b8c51f6e5f30812364f7
Titus <wastingtape@gmail.com>
2025-08-30 12:53:32 -0600

    Fix port assignments

--- packages/lotko/src/ionet.ts
+++ packages/lotko/src/ionet.ts
@@ -4,9 +4,9 @@ export const ioNet = new IoNetwork({
   io3208: IO_3208(0),
 });
 
-export const RightSling = ioNet.defineDevice(({ io3208 }) => {
+export const LeftSling = ioNet.defineDevice(({ io3208 }) => {
   return new SingleCoil({
-    driver: io3208.drivers[7],
+    driver: io3208.drivers[0],
     switch: io3208.switches[31],
     mode: 'pulse',
     initialPwmDurationMs: 20,
